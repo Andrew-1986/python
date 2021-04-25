3e496cc4a4bc8a271571fe8b147885e91a3fa608
Andrew-1986 <plaskalny@gmail.com>
2021-04-25 18:58:08 +0300

    Update hw6.py

--- hw6.py
+++ hw6.py
@@ -29,37 +29,36 @@ def sort_files():
 		if path.is_dir():
 			for files in Path(path).glob("*.png", "*.svg", "*.jpg", "*.jpeg"):
 				file_suffix = files.suffix
-    			file_name = files.rsplit(".", 1)[0]
-    			new_file_name = normalize(filename)
-    			source_path = f'{path}/{files}'
+    				file_name = files.rsplit(".", 1)[0]
+    				new_file_name = normalize(filename)
+    				source_path = f'{path}/{files}'
 				destination_path = f'{path}/images/{new_file_name}.{file_suffix}'
-    			shutil.move(source_path, destination_path)
+    				shutil.move(source_path, destination_path)
 			for files in Path(path).glob("*.avi", "*.mp4", "*.mov", "*.mkv"):
-    			file_suffix = files.suffix
-    			file_name = files.rsplit(".", 1)[0]
-    			new_file_name = normalize(filename)
-    			source_path = f'{path}/{files}'
+    				file_suffix = files.suffix
+    				file_name = files.rsplit(".", 1)[0]
+    				new_file_name = normalize(filename)
+    				source_path = f'{path}/{files}'
 				destination_path = f'{path}/video/{new_file_name}.{file_suffix}'
-    			shutil.move(source_path, destination_path)
-    		for files in Path(path).glob("*.doc", "*.txt", "*.pdf", "*.docx"):
-    			file_suffix = files.suffix
-    			file_name = files.rsplit(".", 1)[0]
-    			new_file_name = normalize(filename)
-    			source_path = f'{path}/{files}'
+    				shutil.move(source_path, destination_path)
+    			for files in Path(path).glob("*.doc", "*.txt", "*.pdf", "*.docx"):
+    				file_suffix = files.suffix
+    				file_name = files.rsplit(".", 1)[0]
+    				new_file_name = normalize(filename)
+    				source_path = f'{path}/{files}'
 				destination_path = f'{path}/documents/{new_file_name}.{file_suffix}'
-    			shutil.move(source_path, destination_path)
-    		for files in Path(path).glob("*.mp3", "*.ogg", "*.wav", "*.arm"):
-    			file_suffix = files.suffix
-    			file_name = files.rsplit(".", 1)[0]
-    			new_file_name = normalize(filename)
-    			source_path = f'{path}/{files}'
+    				shutil.move(source_path, destination_path)
+    			for files in Path(path).glob("*.mp3", "*.ogg", "*.wav", "*.arm"):
+    				file_suffix = files.suffix
+    				file_name = files.rsplit(".", 1)[0]
+    				new_file_name = normalize(filename)
+    				source_path = f'{path}/{files}'
 				destination_path = f'{path}/music/{new_file_name}.{file_suffix}'
-    			shutil.move(source_path, destination_path)
-    		for files in Path(path).glob("*.zip", "*.gz", "*.tar"):
-    			archives.append(files)
-    			archive_format = files.suffix
-    			archive_path = f'{path}/archives/{archive_format}'
-    			shutil.unpack_archive(files, archive_path, archive_format)
+    				shutil.move(source_path, destination_path)
+    			for files in Path(path).glob("*.zip", "*.gz", "*.tar"):
+    				archive_format = files.suffix
+    				archive_path = f'{path}/archives/{archive_format}'
+    				shutil.unpack_archive(files, archive_path, archive_format)
 		else:
 			print(f"{path} is not dir")
 	else:
